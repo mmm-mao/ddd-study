2d1f8210f430e68c311afd184f9d15f5d371e5fc
BIGLOBE <biglobe@BIGLOBE.local>
2015-09-14 12:10:58 +0900

    テーブル設計実施

--- src/main/resources/db/V1__create-schema.sql
+++ src/main/resources/db/V1__create-schema.sql
@@ -1,7 +1,68 @@
+-- 会員
+CREATE TABLE IF NOT EXISTS member_signup_event (
+  user_id VARCHAR2(8) NOT NULL,
+  signup_date DATE NOT NULL,
+  system_receipt_date_time TIMESTAMP NOT NULL,
+  CONSTRAINT pk_member_signup_event PRIMARY KEY(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS member_withdraw_event (
+  user_id VARCHAR2(8) NOT NULL,
+  withdrawal_date DATE NOT NULL,
+  system_receipt_date_time TIMESTAMP NOT NULL,
+  CONSTRAINT pk_member_withdraw_event PRIMARY KEY(user_id)
+);
+
+-- 会員プロフィール
+CREATE TABLE IF NOT EXISTS member_profile_register_event (
+  profile_register_event_id VARCHAR2(13) auto_increment NOT NULL,
+  user_id VARCHAR2(8) NOT NULL,
+  name VARCHAR2(128) NOT NULL,
+  name_kana VARCHAR2(128) NOT NULL,
+  gender VARCHAR2(4) NOT NULL,
+  birthday DATE NOT NULL,
+  zip_code VARCHAR2(8)  NOT NULL,
+  address VARCHAR2(128)  NOT NULL,
+  system_receipt_date_time TIMESTAMP NOT NULL,
+  CONSTRAINT pk_member_profile_register_event PRIMARY KEY(profile_register_event_id)
+);
+
+CREATE TABLE IF NOT EXISTS member_mail_address_register_event (
+  mail_address_register_event_id VARCHAR2(13) auto_increment NOT NULL,
+  user_id VARCHAR2(8) NOT NULL,
+  mail_address VARCHAR2(128) NOT NULL,
+  system_receipt_date_time TIMESTAMP NOT NULL,
+  CONSTRAINT pk_member_mail_address_register_event PRIMARY KEY(mail_address_register_event_id)
+);
+
+
+-- 認証
+
+CREATE TABLE IF NOT EXISTS auth_event (
+  auth_event_id VARCHAR2(13) auto_increment NOT NULL,
+  user_id VARCHAR2(8) NOT NULL,
+  user_password VARCHAR2(128) NOT NULL,
+  CONSTRAINT pk_auth_event PRIMARY KEY(auth_event_id)
+);
+
+
+-- コース
+
+CREATE TABLE IF NOT EXISTS course_apply_event (
+  course_apply_event_id VARCHAR2(13) auto_increment NOT NULL,
+  user_id VARCHAR2(8) NOT NULL,
+  course VARCHAR2(50) NOT NULL,
+  switching_date DATE NOT NULL,
+  CONSTRAINT pk_course_apply_event PRIMARY KEY(course_apply_event_id)
+);
+
+
+-- クレジットカード
+
 CREATE TABLE IF NOT EXISTS credit_register_event (
   credit_card_number VARCHAR2(13) NOT NULL,
   user_id VARCHAR2(8) NOT NULL,
-  system_receipt_date_time DATE NOT NULL,
+  system_receipt_date_time TIMESTAMP NOT NULL,
   CONSTRAINT pk_credit_register_event PRIMARY KEY(credit_card_number)
 );
 
@@ -9,7 +70,8 @@ CREATE TABLE IF NOT EXISTS credit_valid_event (
   credit_valid_event_id VARCHAR2(13) auto_increment NOT NULL,
   credit_card_number VARCHAR2(13) NOT NULL,
   valid_credit_card_status VARCHAR2(4) NOT NULL,
-  system_receipt_date_time DATE NOT NULL,
+  system_receipt_date_time TIMESTAMP NOT NULL,
   CONSTRAINT pk_credit_valid_event PRIMARY KEY(credit_valid_event_id)
 );
+
 
--- src/test/resources/sql/drop-tables.sql
+++ src/test/resources/sql/drop-tables.sql
@@ -1,2 +1,8 @@
+DROP TABLE IF EXISTS member_signup_event;
+DROP TABLE IF EXISTS member_withdraw_event;
+DROP TABLE IF EXISTS member_profile_register_event;
+DROP TABLE IF EXISTS member_mail_address_register_event;
+DROP TABLE IF EXISTS auth_event;
+DROP TABLE IF EXISTS course_apply_event;
 DROP TABLE IF EXISTS credit_register_event;
 DROP TABLE IF EXISTS credit_valid_event;
